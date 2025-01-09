836cd29efad4f1fc883396a2859241aed84cf73e
jedommo <43824473+jedommo@users.noreply.github.com>
2025-01-08 17:59:22 -0800

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,16 @@
+$botToken = "1687364449:AAHpiyYwUPdccB0Ut5g9-1xPKRbfLoPxWbQ";
+$apiUrl = "https://api.telegram.org/bot$botToken/";
+
+
+$update = json_decode(file_get_contents("php://input"), true);
+
+if (isset($update["message"])) {
+    $chatId = $update["message"]["chat"]["id"];
+    $text = $update["message"]["text"];
+
+
+    $reply = "You said: $text"; 
+
+
+    file_get_contents($apiUrl . "sendMessage?chat_id=" . $chatId . "&text=" . urlencode($reply));
+}
